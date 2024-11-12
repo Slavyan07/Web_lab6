5420bd7fdff141c35280f0c872365f9917005a98
Slavyan07 <balzhinimaevslavik1@gmail.com>
2024-11-12 19:59:19 +0700

    Add app market

--- woodmarket/market/views.py
+++ woodmarket/market/views.py
@@ -1,10 +1,3 @@
-from django.http import  HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
- return HttpResponse("Страница приложения market.")
-
-def categories(request):
- return HttpResponse("<h1>Статьи по категориям</h1>")
-
