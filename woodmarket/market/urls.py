4f2acee284ba6163763073637d6098a799056976
Slavyan07 <balzhinimaevslavik1@gmail.com>
2025-05-30 00:15:14 +0700

    Готовая первая лаб. работа

--- woodmarket/market/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import index, exp
-
-
-urlpatterns = [
-    path('', index),
-    path('<str:material>', exp)
-]
--- woodmarket/market/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("Страница приложения market.")
-
-
-def exp(request, material):
-    return HttpResponse(f"Вы выбрали материал: {material}")
